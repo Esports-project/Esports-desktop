785c2509ac71da49432d3644059334ec80b24b5b
RayenBourguiba <58385581+RayenBourguiba@users.noreply.github.com>
2022-04-15 02:43:53 +0100

    Panier + Service Panier

--- src/LevelUp/entities/Panier.java
+++ src/LevelUp/entities/Panier.java
@@ -1,23 +1,26 @@
 package Esprit.entities;
 
 import com.sun.javafx.collections.ObservableMapWrapper;
+
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name = "Lignecommande")
-@XmlAccessorType (XmlAccessType.FIELD)
-public class Panier implements Serializable{
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Panier implements Serializable {
 
     @XmlElement(name = "Ligne")
-    private Map<Produit,Integer> LigneCommande = new HashMap<Produit, Integer>() ;
+    private Map<Produit, Integer> LigneCommande = new HashMap<Produit, Integer>();
 
     public Panier() {
     }
--- src/LevelUp/services/ServicePanier.java
+++ src/LevelUp/services/ServicePanier.java
@@ -1,4 +1,54 @@
 package Esprit.services;
 
+import Esprit.entities.Panier;
+import Esprit.entities.Produit;
+import Esprit.Connection.MyConnection;
+import java.sql.Connection;
+import java.lang.Integer;
+import javafx.collections.ObservableMap;
+
 public class ServicePanier {
+    Connection cnx;
+
+    public static Panier lc = new Panier();
+
+    public ServicePanier() {
+        cnx = MyConnection.getInstance().getConnection();
+    }
+
+    public void ajouterLigneDeCommande(Produit p, int qt) {
+        if (lc.getLignedeCommande().containsKey(p)) {
+            lc.getLignedeCommande().put(p, lc.getLignedeCommande().get(p) + qt);
+        } else {
+            lc.getLignedeCommande().put(p, qt);
+        }
+        System.out.println("Produit ajouté!");
+//        HttpCookie session = new HttpCookie("panier", null);
+//        session = HttpCookie.parse("panier");
+//        System.out.println(session.getValue());
+    }
+
+    public boolean supprimerLigneDecommande(Produit p, int qt) {
+        if(lc.getLignedeCommande().containsKey(p))
+        {
+            if (lc.getLignedeCommande().get(p) >= qt) {
+                lc.getLignedeCommande().put(p, lc.getLignedeCommande().get(p) - qt);
+                if(lc.getLignedeCommande().get(p)<=0)
+                {
+                    lc.getLignedeCommande().remove(p);
+                }
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public ObservableMap<Produit, Integer> consulterLigneDeCommandes() {
+        return (ObservableMap) lc.getLignedeCommande();
+    }
+
+    public void modifierLigneDeCommande(Produit p) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
